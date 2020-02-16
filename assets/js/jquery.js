0220338f904bc0adffebc5ac94a77a89217b9bcf
Ubuntu <ubuntu@ip-172-31-80-87.ec2.internal>
2020-02-16 02:56:42 +0000

    add skills

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,21 +1,43 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
-    $('#img-cook').mouseenter(function (){
-        $(this).removeClass('work-img');
+    $('#cook').mouseenter(function (){
+        $('#img-cook').removeClass('work-img');
     });
-    $(' #img-cook').mouseleave(function (){
-        $(this).addClass('work-img');
+    $('#cook').mouseleave(function (){
+        $('#img-cook').addClass('work-img');
     });
-    $('#img-tetris').mouseenter(function (){
-        $(this).removeClass('work-img');
+    $('#tetris').mouseenter(function (){
+        $('#img-tetris').removeClass('work-img');
     });
-    $(' #img-tetris').mouseleave(function (){
-        $(this).addClass('work-img');
+    $('#tetris').mouseleave(function (){
+        $('#img-tetris').addClass('work-img');
     });
-    $('#img-gym').mouseenter(function (){
-        $(this).removeClass('work-img');
+    $('#gym').mouseenter(function (){
+        $('#img-gym').removeClass('work-img');
     });
-    $(' #img-gym').mouseleave(function (){
-        $(this).addClass('work-img');
+    $('#gym').mouseleave(function (){
+        $('#img-gym').addClass('work-img');
     });
+    
+    $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
   });
