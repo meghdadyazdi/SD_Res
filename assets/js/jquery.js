88f5e148435da5275fba3e22ca2466b8f0d127d2
Ubuntu <ubuntu@ip-172-31-80-87.ec2.internal>
2020-02-16 00:23:04 +0000

    add cards

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('#img-cook').mouseenter(function (){
+        $(this).removeClass('work-img');
+    });
+    $(' #img-cook').mouseleave(function (){
+        $(this).addClass('work-img');
+    });
+    $('#img-tetris').mouseenter(function (){
+        $(this).removeClass('work-img');
+    });
+    $(' #img-tetris').mouseleave(function (){
+        $(this).addClass('work-img');
+    });
+    $('#img-gym').mouseenter(function (){
+        $(this).removeClass('work-img');
+    });
+    $(' #img-gym').mouseleave(function (){
+        $(this).addClass('work-img');
+    });
   });
