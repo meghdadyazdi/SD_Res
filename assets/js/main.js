c094ee615500b9a400eb741a6c05674e5ca4a880
Ubuntu <ubuntu@ip-172-31-80-87.ec2.internal>
2020-03-25 10:23:16 +0000

    fixing some typo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ function sendEmail() {
 	Email.send({
 	Host: "smtp.gmail.com",
 	Username : "meghdad23@gmail.com",
-	Password : "@Megh23yaz",
+	Password : "",
 	To : 'meghdadyazdi@yahoo.com',
 	From : "meghdad23@gmail.com",
 	Subject : "helloommmmmmmmmm",
